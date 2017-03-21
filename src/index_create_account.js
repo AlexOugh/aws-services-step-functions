19e7903474b5ff292ded13a9e205b74549f3ad72
Alex Ough <alex.ough@sungardas.com>
2017-03-21 15:22:16 -0500

    added roles to federate to billing master account

--- src/index_create_account.js
+++ src/index_create_account.js
@@ -3,7 +3,9 @@ exports.handler = (event, context, callback) => {
   //event.account.id = Math.random().toString().replace('0.', '').replace('.', '');
   event.account.id = '808331752250';
   event.account.created_at = new Date().toString();
-  event.federation.roles.push({"roleArn": "arn:aws:iam::" + event.account.id + ":role/OrganizationAccountAccessRole"});
+  var roles = event.billing_master.roles;
+  roles.push({"roleArn": "arn:aws:iam::" + event.account.id + ":role/OrganizationAccountAccessRole"});
+  event.federation.roles = roles;
   event.federation.authorizer_user_guid = "808331752250";
   callback(null, event);
 };
